67c9d6c303c4e8e93fc2cc745c01acb457e15603
Jason Tsai <jason@pews.dev>
2024-05-07 21:08:38 +0800

    refactor: fix clippy clone assignment (#191)
    * refactor: fix clippy
    
    * remove dbg
    
    ---------
    
    Co-authored-by: Amr Bashir <amr.bashir2015@gmail.com>

--- src/platform_impl/gtk/mod.rs
+++ src/platform_impl/gtk/mod.rs
@@ -19,7 +19,7 @@ use accelerator::{from_gtk_mnemonic, parse_accelerator, to_gtk_mnemonic};
 use gtk::{gdk, prelude::*, AboutDialog, Container, Orientation};
 use std::{
     cell::RefCell,
-    collections::HashMap,
+    collections::{hash_map::Entry, HashMap},
     rc::Rc,
     sync::atomic::{AtomicBool, Ordering},
 };
@@ -264,9 +264,9 @@ impl Menu {
 
         // This is the first time this method has been called on this window
         // so we need to create the menubar and its parent box
-        if self.gtk_menubars.get(&id).is_none() {
+        if let Entry::Vacant(e) = self.gtk_menubars.entry(id) {
             let menu_bar = gtk::MenuBar::new();
-            self.gtk_menubars.insert(id, menu_bar);
+            e.insert(menu_bar);
         } else {
             return Err(crate::Error::AlreadyInitialized);
         }
@@ -759,7 +759,7 @@ impl MenuChild {
 /// IconMenuItem methods
 impl MenuChild {
     pub fn set_icon(&mut self, icon: Option<Icon>) {
-        self.icon = icon.clone();
+        self.icon.clone_from(&icon);
 
         let pixbuf = icon.map(|i| i.inner.to_pixbuf_scale(16, 16));
         for items in self.gtk_menu_items.borrow().values() {
